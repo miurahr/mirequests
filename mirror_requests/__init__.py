10af552a21b8108e3ef9e93ef5171c14780b26a4
Hiroshi Miura <miurahr@linux.com>
2019-05-26 16:01:51 +0900

    Take blaclist as a get parameter
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- mirror_requests/__init__.py
+++ mirror_requests/__init__.py
@@ -7,25 +7,33 @@ from . import exceptions
 
 __all__ = ['get', 'exceptions']
 
-_blacklist = ['http://mirrors.tuna.tsinghua.edu.cn']
+_default_blacklist = ['http://mirrors.tuna.tsinghua.edu.cn']
 
 
-def _in_blacklist(url):
-    for b in _blacklist:
+def _in_blacklist(url, blacklist=None):
+    if blacklist is None:
+        blacklist = _default_blacklist
+    for b in blacklist:
         if url.startswith(b):
             return True
     return False
 
 
-def get(url, stream=False):
-    r = requests.get(url, stream=stream, allow_redirects=False)
+def get(url, params=None, blacklist=None, **kwargs):
+    kwargs.setdefault('allow_redirects', True)
+    original_allow_redirects = kwargs['allow_redirects']
+    kwargs['allow_redirects'] = False
+    r = requests.request('get', url, params=params, **kwargs)
+    if not original_allow_redirects:
+        return r
     if r.status_code == 302:
         # tsinghua.edu.cn is problematic and it prohibit service to specific geo location.
         # we will use another redirected location for that.
         newurl = r.headers['Location']
-        if _in_blacklist(newurl):
-            mml = mirrors.Metalink(url, blacklist=_blacklist)
+        if _in_blacklist(newurl, blacklist):
+            mml = mirrors.Metalink(url, blacklist=blacklist)
             newurl = mml.altlink()
-        r = requests.get(newurl, stream=stream)
+        kwargs['allow_redirects'] = True
+        r = requests.request('get', newurl, params=params, **kwargs)
     return r
 
