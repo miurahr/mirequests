9d36cc77d212b11128c7655e25ed352bc7dc0eb3
Hiroshi Miura <miurahr@linux.com>
2019-05-26 20:44:04 +0900

    Add more test case
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- mirror_requests/tests/test_basic.py
+++ mirror_requests/tests/test_basic.py
@@ -8,3 +8,9 @@ def test_basic_get():
     r = mirror_requests.get(url)
     assert r is not None
 
+
+@pytest.mark.basic
+def test_basic_get_redirect():
+    url = 'https://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_53/Updates.xml'
+    r = mirror_requests.get(url)
+    assert r is not None
